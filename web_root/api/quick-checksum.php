2eacccb3813240d357b748b2c83b600c5ed1cfef
James Elstone <32575806+JamesElstone@users.noreply.github.com>
2026-06-22 19:36:19 +0100

    Trace quick checksum API

--- web_root/api/quick-checksum.php
+++ web_root/api/quick-checksum.php
@@ -9,8 +9,50 @@ declare(strict_types=1);
 
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . 'bootstrap.php';
 
+if (!function_exists('quick_checksum_trace_request_start')) {
+    function quick_checksum_trace_request_start(): void
+    {
+        logDetails();
+    }
+
+    function quick_checksum_trace_request_ready(): void
+    {
+        logDetails();
+    }
+
+    function quick_checksum_trace_service_ready(): void
+    {
+        logDetails();
+    }
+
+    function quick_checksum_trace_handle_start(): void
+    {
+        logDetails();
+    }
+
+    function quick_checksum_trace_response_ready(): void
+    {
+        logDetails();
+    }
+
+    function quick_checksum_trace_response_sent(): void
+    {
+        logDetails();
+    }
+}
+
+quick_checksum_trace_request_start();
+
 $headers = function_exists('getallheaders') ? (array)getallheaders() : [];
 $request = new RequestFramework($_GET, $_POST, $_SERVER, $_FILES, $headers, null, $_COOKIE);
+quick_checksum_trace_request_ready();
 
 $service = new SwallowtailQuickChecksumApiService();
-$service->handleCheck($request)->send();
+quick_checksum_trace_service_ready();
+
+quick_checksum_trace_handle_start();
+$response = $service->handleCheck($request);
+quick_checksum_trace_response_ready();
+
+$response->send();
+quick_checksum_trace_response_sent();
